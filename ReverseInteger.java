1099372742ee2df4a7b12ad2d515858fe830f3e0
SupremXH <43590279+SupremXH@users.noreply.github.com>
2019-12-29 17:36:33 -0500

    Add files via upload

---
+++ ReverseInteger.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int reverse(int x) {
+		int val=0;
+		while(x!=0) {
+			int rem=x%10;
+			x/=10;
+            if(val>Integer.MAX_VALUE/10 || val<Integer.MIN_VALUE/10) return 0;
+			val=val*10+rem;
+		}
+		return val;
+	}
+}
