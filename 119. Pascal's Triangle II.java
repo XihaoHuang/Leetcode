008f156f09b302d57a4563959e8d949a7e8c3bfb
SupremXH <43590279+SupremXH@users.noreply.github.com>
2020-08-13 11:37:47 -0400

    Add files via upload

--- 119. Pascal's Triangle II.java
+++ 119. Pascal's Triangle II.java
@@ -1,24 +1,30 @@
 class Solution {
-    public List<Integer> getRow(int rowIndex) {
-        List<Integer> al=new ArrayList<Integer>();
-        for(int r=0;r<=rowIndex;r++){
-            if(r==0){
-                al.add(1);
-            }
-            else if(r==1){
-                al=new ArrayList<>();
-                al.add(1);
-                al.add(1);   
-            }else{
-                List<Integer> temp=new ArrayList<>();
-                temp.add(1);
-                for(int i=0;i<=al.size()-2;i++){
-                    temp.add(al.get(i)+al.get(i+1));
-                }
-                temp.add(1);
-                al=temp;
-            }
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int[] dp=new int[triangle.size()];
+        dp[0]=triangle.get(0).get(0);
+        if(triangle.size()==1) return dp[0];
+        return findmin(triangle,dp,1);
+
+    }
+    public int  findmin(List<List<Integer>> tr,int[] dp,int row){
+        List<Integer> al=new ArrayList<>();
+        al=tr.get(row);
+        int left=dp[0],right;
+        dp[0]+=al.get(0);
+        for(int c=1;c<row;c++){
+            right=dp[c];
+            dp[c]=Math.min(left,right)+al.get(c);
+            left=right;
         }
-        return al;
+        dp[row]=left+al.get(row);
+        // System.out.println(Arrays.toString(dp));
+        if(row==tr.size()-1){
+            int res=dp[0];
+            for(int i=1;i<dp.length;i++){
+                res=Math.min(res,dp[i]);
+            }
+            return res;
+        }
+    return findmin(tr,dp,row+1);
     }
 }
