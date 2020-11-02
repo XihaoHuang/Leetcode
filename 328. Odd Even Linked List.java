56af4ba867b6c9be6e3121bfc8ec9d0b414b8278
SupremXH <43590279+SupremXH@users.noreply.github.com>
2020-11-02 16:46:43 -0500

    Add files via upload

--- 328. Odd Even Linked List.java
+++ 328. Odd Even Linked List.java
@@ -10,25 +10,16 @@
  */
 class Solution {
     public ListNode oddEvenList(ListNode head) {
-        if(head==null) return head;
-        ListNode Evenhead=new ListNode(0);
-        int i=1;
-        ListNode cur=head;
-        while(cur!=null && cur.next!=null){
-                ListNode even=cur.next;
-                cur.next=even.next;
-                even.next=null;
-                ListNode c=Evenhead;
-                while(c.next!=null)
-                    c=c.next;
-                c.next=even;
-                cur=cur.next;
-            
-        }
-        cur=head;
-        while(cur.next!=null)
-            cur=cur.next;
-        cur.next=Evenhead.next;
+       if(head!=null){
+           ListNode odd=head,even=head.next,Evenhead=even;
+           while(even!=null && even.next!=null){
+               odd.next=odd.next.next;
+               even.next=even.next.next;
+               odd=odd.next;
+               even=even.next;
+           }
+           odd.next=Evenhead;
+       }
         return head;
-    }
+    } 
 }
