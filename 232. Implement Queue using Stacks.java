8b0708a6ac3b65b5d5db1937c6ee2bd9216fa2a2
SupremXH <43590279+SupremXH@users.noreply.github.com>
2020-11-17 12:19:30 -0500

    Add files via upload

---
+++ 232. Implement Queue using Stacks.java
@@ -0,0 +1,43 @@
+class MyQueue {
+    Stack<Integer> stack;
+    /** Initialize your data structure here. */
+    public MyQueue() {
+        stack=new Stack<>();
+    }
+    
+    /** Push element x to the back of queue. */
+    public void push(int x) {
+        Stack<Integer> s=new Stack<>();
+        while(!stack.isEmpty()){
+            s.push(stack.pop());
+        }
+        s.push(x);
+        while(!s.isEmpty()){
+            stack.push(s.pop());
+        }
+    }
+    
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+        return stack.pop();        
+    }
+    
+    /** Get the front element. */
+    public int peek() {
+        return stack.peek();
+    }
+    
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+        return stack.isEmpty();
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
