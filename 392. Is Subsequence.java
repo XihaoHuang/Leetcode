8994ca32059396d7022e18f6f1a542fd683b42c3
SupremXH <43590279+SupremXH@users.noreply.github.com>
2021-05-08 13:40:48 -0400

    Add files via upload

---
+++ 392. Is Subsequence.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int index=0;
+        for(char c:t.toCharArray()){
+            if(index<s.length() && c==s.charAt(index))
+                index++;
+        }
+        return index==s.length();
+    }
+}
