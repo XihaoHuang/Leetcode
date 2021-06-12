b3230525f938eee27eddfc50a182a6c9e784c73d
SupremXH <43590279+SupremXH@users.noreply.github.com>
2021-06-12 12:59:10 -0400

    Add files via upload

---
+++ 1155. Number of Dice Rolls With Target Sum.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numRollsToTarget(int d, int f, int target) {
+        if(target>f*d) return 0;
+        int mod=(int)Math.pow(10,9)+7;
+        int[][] dp=new int [d+1][target+1];
+        dp[0][0]=1;
+        for(int i=1;i<=d;i++){
+            for(int j=0;j<=target;j++){
+                for(int k=1;k<=f;k++){
+                    if(j-k<0) continue;
+                    dp[i][j]=(dp[i][j]+dp[i-1][j-k])%mod;
+                }
+            }
+        }
+        return dp[d][target];
+    }
+} 
