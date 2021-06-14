67a73a3dd5065569e36d0100035b3b217ef08376
SupremXH <43590279+SupremXH@users.noreply.github.com>
2021-06-14 14:40:56 -0400

    Add files via upload

--- 561. Array Partition I.java
+++ 561. Array Partition I.java
@@ -1,10 +1,9 @@
 class Solution {
     public int arrayPairSum(int[] nums) {
         Arrays.sort(nums);
-        int res=0;
-        for(int i=0;i<nums.length;i+=2){
-            res+=nums[i];
-        }
-        return res;
+        int sum=0;
+        for(int i=0;i<nums.length;i+=2)
+            sum+=nums[i];
+        return sum;
     }
 }
