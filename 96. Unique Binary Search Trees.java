c448d6a6b9a28b00d7af8f295ef4726d083a3d40
SupremXH <43590279+SupremXH@users.noreply.github.com>
2021-06-23 12:30:29 -0400

    Add files via upload

--- 96. Unique Binary Search Trees.java
+++ 96. Unique Binary Search Trees.java
@@ -1,12 +1,21 @@
 class Solution {
-    public int numTrees(int n) {
-        int[] dp=new int[n+1];
-        dp[0]=dp[1]=1;
-        for(int i=2;i<=n;i++){
-            for(int j=0;j<i;j++){
-                dp[i]+=dp[j]*dp[i-j-1];
+    public boolean isInterleave(String s1, String s2, String s3) {
+        int l1=s1.length();
+        int l2=s2.length();
+        if((l1+l2)!=s3.length()) return false;
+        boolean[][] dp=new boolean[l1+1][l2+1];
+        dp[0][0]=true;
+        for(int i=1;i<=l2;i++)
+            dp[0][i]=dp[0][i-1] && (s2.charAt(i-1)==s3.charAt(i-1));
+        for(int i=1;i<=l1;i++)
+            dp[i][0]=dp[i-1][0] && (s1.charAt(i-1)==s3.charAt(i-1));
+        for(int i=1;i<=l1;i++){
+            for(int j=1;j<=l2;j++){
+                dp[i][j]=(dp[i][j-1]&&s2.charAt(j-1)==s3.charAt(i+j-1) || dp[i-1][j]&& s1.charAt(i-1)==s3.charAt(i+j-1));
+                System.out.print(dp[i][j]);
             }
+            System.out.println();
         }
-        return dp[n];
+        return dp[l1][l2];
     }
 }
