a4473d549852d993bb50ab4e48a5849608d46e7e
SupremXH <43590279+SupremXH@users.noreply.github.com>
2021-07-01 19:23:32 -0400

    Add files via upload

---
+++ 397. Integer Replacement.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int integerReplacement(int n) {
+        if(n==1) return 0;
+        if(n%2==0) return 1+integerReplacement(n/2);
+        else return 2+Math.min(integerReplacement(n/2+1),integerReplacement(n/2));
+    }
+}
