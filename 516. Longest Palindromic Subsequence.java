b576906a60f0ad5a8bcaf469696f780f1f76f83d
SupremXH <43590279+SupremXH@users.noreply.github.com>
2021-07-03 15:39:42 -0400

    Add files via upload

---
+++ 516. Longest Palindromic Subsequence.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int longestPalindromeSubseq(String s) {
+        int n=s.length();
+        int[][] dp=new int[n][n];
+        for(int i=0;i<n;i++){
+            dp[i][i]=1;
+            for(int j=i-1;j>=0;j--){
+                if(s.charAt(j)==s.charAt(i)){
+                    dp[j][i]=dp[j+1][i-1]+2;
+                }else{
+                    dp[j][i]=Math.max(dp[j+1][i],dp[j][i-1]);
+                }
+            }
+        }
+        return dp[0][n-1];
+    }
+}
